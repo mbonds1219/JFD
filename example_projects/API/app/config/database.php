13b3086be59a2d57a079a25c1801cde6e0e882f7
root <root@michaelbonds.me>
2015-02-24 19:38:08 -0600

    change to config

--- example_projects/API/app/config/database.php
+++ example_projects/API/app/config/database.php
@@ -48,7 +48,7 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'javascript_for_designerssud',
+            'database'  => 'javascript_for_designers',
             'username'  => $_ENV['MYSQL_USERNAME'],
             'password'  => $_ENV['MYSQL_PASSWORD'],
             'charset'   => 'utf8',
