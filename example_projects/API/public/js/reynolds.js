225afff7418ac0521c830c061062469ce776c2ff
Blackadder331 <reynolds.sharp@gmail.com>
2015-02-24 19:44:34 -0600

    fade out icon

---
+++ example_projects/API/public/js/reynolds.js
@@ -0,0 +1,7 @@
+ $(document).ready(function() {
+   $('.icon').on('mouseenter', function(){
+       $(this).fadeTo('fast', 0.5);
+   });
+    $('icon').on('mouseleave', function(){
+       $(this).fadeTo('fast', 1);
+   }); });
